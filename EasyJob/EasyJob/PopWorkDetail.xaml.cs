50017d68160453420f9ef3796646bbbba8d5e996
Nathachanon <intern002@builk.com>
2019-11-12 17:08:11 +0700

    เพิ่ม popup WorkDetail หน้า Main
    - แก้ ui อื่นๆนิดหน่อย 
    - ใส่ icon popup ที่ไอพีทำ

---
+++ EasyJob/EasyJob/PopWorkDetail.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+
+namespace EasyJob
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PopWorkDetail 
+    {
+        public PopWorkDetail()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            DetailMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(13.916899681091309, 100.49309539794922),
+                Distance.FromKilometers(1)));
+
+            var pin = new Pin
+            {
+                Position = new Position(13.916899681091309, 100.49309539794922),
+                Label = "สถานที่ทำงาน",
+                Address = "เอา loc_name มาโชว์ "
+            };
+
+            DetailMap.Pins.Add(pin);
+        }
+    }
+}

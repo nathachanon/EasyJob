78d613397faa0f932aca1e4e61e8b34f612c6ba6
Nathachanon <intern002@builk.com>
2019-11-14 16:00:22 +0700

    Page Loading
    - เพิ่ม page loading ตอนสมัครสมาชิก
    - แก้ รูปตอนสมัคร ให้เปนวงกลม

--- EasyJob/EasyJob.Android/MainActivity.cs
+++ EasyJob/EasyJob.Android/MainActivity.cs
@@ -7,18 +7,23 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Android;
+using Plugin.Permissions;
+using Android.Support.V4.Content;
 
 namespace EasyJob.Droid
 {
     [Activity(Label = "EasyJob", Icon = "@mipmap/AppIcon3", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        const int RequestLocationId = 0;
+        const int RequestPermission = 0;
 
         readonly string[] LocationPermissions =
         {
             Manifest.Permission.AccessCoarseLocation,
-            Manifest.Permission.AccessFineLocation
+            Manifest.Permission.AccessFineLocation,
+            Manifest.Permission.ReadExternalStorage,
+            Manifest.Permission.WriteExternalStorage
+            
         };
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -28,15 +33,16 @@ namespace EasyJob.Droid
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-                 Rg.Plugins.Popup.Popup.Init(this, savedInstanceState);
+            Rg.Plugins.Popup.Popup.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             Xamarin.FormsMaps.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-            if (requestCode == RequestLocationId)
+            if (requestCode == RequestPermission)
             {
                 if ((grantResults.Length == 1) && (grantResults[0] == (int)Permission.Granted))
                 {
@@ -59,14 +65,15 @@ namespace EasyJob.Droid
 
             if ((int)Build.VERSION.SdkInt >= 23)
             {
-                if (CheckSelfPermission(Manifest.Permission.AccessFineLocation) != Permission.Granted)
+                if (CheckSelfPermission(Manifest.Permission.AccessFineLocation) != Permission.Granted || CheckSelfPermission(Manifest.Permission.ReadExternalStorage) != Permission.Granted)
                 {
-                    RequestPermissions(LocationPermissions, RequestLocationId);
+                    RequestPermissions(LocationPermissions, RequestPermission);
                 }
                 else
                 {
-                    Console.WriteLine("Location permissions already granted.");
+                    Console.WriteLine("permissions already granted.");
                 }
+                
             }
         }
 
